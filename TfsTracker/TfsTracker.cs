f8e810de61a9c4a1866ca9f181ba15269d7e43a7
antoniopan <franciscojavieryeste@hotmail.com>
2020-09-11 11:22:08 +0800

    P0 Only for DSA

--- TfsTracker/TfsTracker.cs
+++ TfsTracker/TfsTracker.cs
@@ -425,7 +425,7 @@ namespace TFS_TRACKER
                     ResolvedDate = Convert.ToDateTime(wi["Resolved Date"].ToString(), new System.Globalization.DateTimeFormatInfo()),
                     NodeName = wi.NodeName,
                     ResolvedBy = iIndex > 0 ? sResolvedBy.Substring(0, sResolvedBy.IndexOf('_')) : "",
-                    Reserved = (wi.Type.Name == "Task") ? String.Format("P{0}", wi["Priority"].ToString()) : wi["uAttribute"].ToString()
+                    Reserved = (wi.Type.Name == "Task") ? String.Format("P{0}", wi["Priority"].ToString()) : wi.IterationPath
                 });
             }
 
