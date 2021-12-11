eac5efbc8f65a740f3ff928c077f28cf20be41ac
antoniopan <franciscojavieryeste@hotmail.com>
2021-12-11 17:18:07 +0800

    update

--- MailSync/UIDeal_B69SP4.py
+++ MailSync/UIDeal_B69SP4.py
@@ -15,18 +15,18 @@ def modify_html(src_htm, dst_htm, src_xls, name_file, option):
     xls = xlrd.open_workbook(src_xls)
 
     # 读取自测Task
-    task_sheet = xls.sheet_by_name("Task Expired")
+    task_sheet = xls.sheet_by_name("Design Task Expired")
     modify_table.fill_html_with_blank_row(tables[0], task_sheet.nrows)
     modify_table.fill_html_from_sheet(task_sheet, tables[0])
 
     # 读取未评审H3 Improvement Task
-    task_sheet = xls.sheet_by_name("H3 Task Unreviewed")
-    modify_table.fill_html_with_blank_row(tables[1], task_sheet.nrows)
-    modify_table.fill_html_from_sheet(task_sheet, tables[1])
+    #task_sheet = xls.sheet_by_name("H3 Task Unreviewed")
+    #modify_table.fill_html_with_blank_row(tables[1], task_sheet.nrows)
+    #modify_table.fill_html_from_sheet(task_sheet, tables[1])
 
     # 读取本周解决 Task
-    task_sheet = xls.sheet_by_name("H3 Task This Week")
-    modify_table.fill_html_with_blank_row(tables[2], task_sheet.nrows)
+    task_sheet = xls.sheet_by_name("Design Task This Week")
+    modify_table.fill_html_with_blank_row(tables[1], task_sheet.nrows)
     modify_table.fill_html_from_sheet(task_sheet, tables[2])
 
     if os.path.exists(dst_htm):
@@ -41,7 +41,7 @@ def modify_html(src_htm, dst_htm, src_xls, name_file, option):
         mail.To = s_name
         mail.Recipients.Add('liangliang.pan_HSW-GS')
         mail.CC = 'HSW_GS_IPA_AP@united-imaging.com'
-        mail.Subject = 'UIDealB69SP4H3软件状态同步%s' % (time.strftime('%Y-%m-%d', time.localtime()))
+        mail.Subject = 'UIDealB69SP4H4软件状态同步%s' % (time.strftime('%Y-%m-%d', time.localtime()))
         mail.HTMLBody = soup.prettify()
         mail.Send()
     elif option == 1:
